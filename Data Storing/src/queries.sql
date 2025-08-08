daaeb9e42129de310938e1f2e16355ccbbef8cdd
millicecup <147978444+millicecup@users.noreply.github.com>
2025-08-08 11:22:12 +0700

    feat:add queries code for checking data storage

---
+++ Data Storing/src/queries.sql
@@ -0,0 +1,5 @@
+select * from ingredients where quantity = '2/3' limit 5;
+select * from recipes where total_time < 30;
+select * from categories;
+select * from instructions where text like '%stiff peaks%';
+select * from recipes where title like '%chocolate%';
